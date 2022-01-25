8e01ef0e9a8979d824da8c424935b4339628bd3f
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-25 10:10:09 -0900

    add MudTile WIP

--- EFMRN2/Controllers/MapController.cs
+++ EFMRN2/Controllers/MapController.cs
@@ -7,60 +7,143 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EFMRN2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFMRN2.Controllers
 {
-    public class MapController : Controller
+  public class MapController : Controller
+  {
+    private readonly EFMRN2Context _db;
+    public static List<MapController> list = new List<MapController>();
+    public MapController(EFMRN2Context db)
     {
-        private readonly EFMRN2Context _db;
-
-        public MapController(EFMRN2Context db)
-        {
-            _db = db;
-        }
-        public void TileAction(Player one)
-        {
-          Tile target = GetPlayerTile(one);
-          switch(target.Method)
-          {
-            case 1:
-            //the doing
-            Door(one);
-            break;
-            case 2:
-            break;
-            default:
-            break;
-          }
-
-          
-        }
-
-        //A route to get new map tiles( When the player moves north get the new north row.)
-        public Tile GetPlayerTile(Player p)
-        {
-          Tile target = _db.Map.FirstOrDefault(t=>t.X==p.X&&t.Y==p.Y&&t.Z==p.Z);
-          return target;
-        }
-        public Tile GetTileById(int id)
-        {
-          Tile target = _db.Map.FirstOrDefault(t=>t.TileId == id);
-          return target;
-        }
-        public void SetPlayerPos(Player p, Tile t)
-        {
-          p.X = t.X;
-          p.Y = t.Y;
-          p.Z = t.Z;
-        }
-        public void Door(Player p)
-        {
-          Tile targetA = GetPlayerTile(p);
-          int i = Int32.Parse(targetA.Aux);
-          Tile target = GetTileById(i);
-          SetPlayerPos(p,target);
-        }
+        _db = db;
+        MapController.list.Add(this);
+    }
+    //Still have to call this when player moves
+    public void TileAction(Player one, Tile targetTile)
+    {
+      Tile target = GetPlayerTile(one);
+      switch(target.Method)
+      {
+        case 1:
+        //the doing
+        Door(one);
+        break;
+        case 2:
+        MudTile(one, targetTile);
+        break;
+        default:
+        break;
+      }
     }
 
     
+    public Tile GetPlayerTile(Player p)
+    {
+      Tile target = _db.Map.FirstOrDefault(t=>t.X==p.X&&t.Y==p.Y&&t.Z==p.Z);
+      return target;
+    }
+    public Tile GetTileById(int id)
+    {
+      Tile target = _db.Map.FirstOrDefault(t=>t.TileId == id);
+      return target;
+    }
+    public void SetPlayerPos(Player p, Tile t)
+    {
+      Player target =_db.Players.FirstOrDefault(pl=>pl.PlayerId == p.PlayerId);
+      target.X = t.X;
+      target.Y = t.Y;
+      target.Z = t.Z;
+      _db.Entry(target).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    public void OpenTile(Tile t)
+    {
+      t.Transparent = true;
+      //replace with texture name of open door
+      t.Texture = "walkable";
+      t.Aux = "open";
+      _db.Entry(t).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    public void CloseTile(Tile t)
+    {
+      t.Transparent = true;
+      //replace with texture name of closed door
+      t.Texture = "walkable";
+      t.Aux = "closed";
+      _db.Entry(t).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    //teleport to plain tile in front of door
+    public void Door(Player p)
+    {
+      Tile targetA = GetPlayerTile(p);
+      int i = Int32.Parse(targetA.Aux);
+      Tile target = GetTileById(i);
+      SetPlayerPos(p,target);
+    }
+    //While occupied aux tile is transparent and has x texture,
+    //after moving off aux tile reverts
+    //
+    public void PressurePlateKey(Player p)
+    {
+      Tile targetA = GetPlayerTile(p);
+      int i = Int32.Parse(targetA.Aux);
+      Tile target = GetTileById(i);
+      if(target.Aux == "closed")
+      {
+        OpenTile(target);
+      }
+      if(target.Aux == "open")
+      {
+        CloseTile(target);
+      }
+    }
+        /*
+         tile A = tile standing on
+         tile B = tile to move to
+          
+          get input directions
+          check tile B
+          if tile B walkable
+          set var to tile B
+          do action on tile A
+          and the move to tile B
+
+          then after move do action on tile you are now on
+        */
+
+        /*
+          If you are on mud and the target tile is mud change the target tiles aux to MOVE BACK 
+          and then if the targetTile is MOVE BACK TO X Y Z, do that and change aux back to mud
+        */
+    public bool MudTile(Player p, Tile targetTile)
+    {
+      Tile targetA = GetPlayerTile(p);
+      if(targetA.Aux == "mud" && targetTile.Aux == "mud")
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
 }
+/*
+  Tiles: 
+  
+  Door Tile
+  AUX = Position to move to, in front of other door
+
+  PressurePlate
+  Aux = targetBlocked tile
+  Aux of target blocked tile = closed
+  also need to figure out exact texture names for open and closed
+
+  Mud tile
+  Aux = mud
+*/
--- EFMRN2/Controllers/PlayerController.cs
+++ EFMRN2/Controllers/PlayerController.cs
@@ -29,18 +29,22 @@ namespace EFMRN2.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index", "Home");
     }
-    public ActionResult MovePlayer(Player player, bool n, bool s, bool e, bool w)
+    public void MovePlayer(Player player, bool n, bool s, bool e, bool w)
     {
       var Destination = GetDestination(player, n, s, e, w);
+      Tile targetTile;
       if(CheckTransparency(Destination[0],Destination[1],Destination[2]))
       {
+        targetTile = _db.Map.FirstOrDefault(t=>t.X==Destination[0]&&t.Y==Destination[1]&&t.Z==Destination[2]);
+        MapController.list[1].TileAction(player, targetTile);
         player.X = Destination[0];
         player.Y = Destination[1];
         player.Z = Destination[2];
         _db.Entry(player).State = EntityState.Modified;
         _db.SaveChanges();
       }
-      return RedirectToAction("TileAction","Map", new {p=player});
+      targetTile = null;
+      MapController.list[1].TileAction(player, targetTile);
     }
     public bool CheckTransparency(int x, int y, int z)
     {
@@ -93,3 +97,5 @@ namespace EFMRN2.Controllers
     }
   }
 }
+
+
