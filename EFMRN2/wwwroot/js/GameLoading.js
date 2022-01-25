12ffce34094e1ef19de0c792e3d1335fc7d49df5
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-25 11:10:59 -0800

    add styles... and prep for merge
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- EFMRN2/wwwroot/js/GameLoading.js
+++ EFMRN2/wwwroot/js/GameLoading.js
@@ -1,17 +1,28 @@
 let canvas = document.getElementById("DisplayCanvas");
 let brush = canvas.getContext("2d");
+let TilesDavis = document.getElementById("TileDivs");
 
-canvas.parentNode.style.height = window.innerHeight;
+let dim = 7;
+
+canvas.parentNode.style.height = window.innerHeight*0.8+"px";
 canvas.parentNode.style.width = canvas.parentNode.style.height;
-canvas.height = canvas.parentNode.style.height;
-canvas.width = canvas.height;
-let q = canvas.height/11;
+canvas.style.height = canvas.parentNode.style.height;
+canvas.style.width = canvas.style.height;
+TilesDavis.style.width = canvas.style.height;
+TilesDavis.style.height = TilesDavis.style.width;
+TilesDavis.style.display = "grid";
+TilesDavis.style.gridTemplateColumns="repeat("+dim+",1fr)";
+canvas.style.position = "absolute";
+
+let q = canvas.height/dim;
 
 
 async function updateMap()
 {
-  let IncomingData = document.getElementId("TileDivs").childNodes;
+  let IncomingData = document.getElementId("TileDivs").children;
   IncomingData.Map(function(square) {
+    square.style.width = q+"px";
+    square.style.height = q+"px";
     let coVariables = convertLinear(parseInt(square.id));
     let target = getTile(coVariables[0],coVariables[1],coVariables[2]);
     square.classList = target.Texture;
@@ -26,7 +37,7 @@ function convertLinear(x,l)
 }
 
 //get request to obtain a tile from a specific location (ajax request to the game controller)
-static async function getTile(x,y,z)
+async function getTile(x,y,z)
 {
   try{
     const response = await fetch(`https://localhost:5000/api/tile/?x=${X},y=${y},z=${z}`);
