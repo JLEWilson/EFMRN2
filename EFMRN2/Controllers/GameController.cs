1dc6110e9bf702597086e22735b1b724e9d6590e
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-01-25 21:11:14 -0800

    add div canvas functionality, and implement follow camera

--- EFMRN2/Controllers/GameController.cs
+++ EFMRN2/Controllers/GameController.cs
@@ -19,95 +19,120 @@ namespace EFMRN2.Controllers
     {
       _db = db;
     }
+
+
   [HttpGet]
   public ActionResult<Tile> Get(int x, int y, int z)
   {
-    Tile target = _db.Map.AsQueryable().Where(t=>t.X == x && t.Y == y && t.Z == z).FirstOrDefault();
+    Tile target = _db.Map.AsQueryable()
+      .Where(t=>t.X == x && t.Y == y && t.Z == z)
+      .FirstOrDefault();
     return target;
   }
+  [HttpGet("fMap")]
+  public ActionResult<IEnumerable<Tile>> GetMap(int pid, int range)
+  {
+    Player tp = _db.Players.FirstOrDefault(p=>p.PlayerId == pid);
+    List<Tile> target = _db.Map.AsQueryable()
+      .Where(t=>(t.X>=(tp.X-range)&&t.X<=(tp.X+range))&&(t.Y>=(tp.Y-range)&&t.Y<=(tp.Y+range))).OrderBy(t=>t.Y).ThenBy(t=>t.X).ToList();
+    return target;
+  }
+
   [HttpGet("AllPlayers")]
   public async Task<ActionResult<IEnumerable<Player>>> GetAllPlayer()
   {
     
     return await _db.Players.ToListAsync();
   }
+
+
   [HttpGet("player")]
   public async Task<ActionResult<Player>> GetPlayer(int id)
   {
     Player target = await _db.Players.FindAsync(id);
     return target;
   }
-  public void MovePlayer(int playerId, bool n, bool s, bool e, bool w)
+
+
+  [HttpGet("move")]
+  public async Task<ActionResult<Player>> MovePlayer(int pid, bool n, bool s, bool e, bool w)
   {
-    Player player = FindPLayer(playerId);
-    var Destination = GetDestination(player, n, s, e, w);
-    Tile targetTile;
+    int[] Destination = GetDestination(pid, n, s, e, w);
+    
     if(CheckTransparency(Destination[0],Destination[1],Destination[2]))
     {
-      targetTile = _db.Map.FirstOrDefault(t=>t.X==Destination[0]&&t.Y==Destination[1]&&t.Z==Destination[2]);
-      MapController.list[1].TileAction(player, targetTile);
-      player.X = Destination[0];
-      player.Y = Destination[1];
-      player.Z = Destination[2];
-      _db.Entry(player).State = EntityState.Modified;
-      _db.SaveChanges();
+      Player target = await _db.Players.FindAsync(pid);
+
+      // Tile targetTile = _db.Map.FirstOrDefault(t=>t.X==Destination[0]&&t.Y==Destination[1]&&t.Z==Destination[2]);
+
+      // MapController.list[1].TileAction(target, targetTile);
+      target.X = Destination[0];
+      target.Y = Destination[1];
+      target.Z = Destination[2];
+      _db.Entry(target).State = EntityState.Modified;
+      await _db.SaveChangesAsync();
+      // MapController.list[1].TileAction(target, targetTile);
     }
-    targetTile = null;
-    MapController.list[1].TileAction(player, targetTile);
+    return RedirectToAction("GetPlayer", new {id = pid});
   }
+
+  
     public bool CheckTransparency(int x, int y, int z)
     {
-      var target = _db.Map.AsQueryable();
-      target= target.Where(t=>t.X == x&&t.Y==y&&t.Z==z);
-      Tile output = target.FirstOrDefault();
-      return output.Transparent;
-        
+      // var target = _db.Map.AsQueryable();
+      // target= target.Where(t=>t.X == x&&t.Y==y&&t.Z==z);
+      // Tile output = target.FirstOrDefault();
+      // return output.Transparent;
+        return _db.Map.FirstOrDefault(t=>t.X==x&&t.Y==y&&t.Z==z).Transparent;
     }
+
+
+
     public Player FindPLayer(int playerId)
     {
       Player player = _db.Players.FirstOrDefault(p => p.PlayerId == playerId);
       return player;
     }
-    public int[] GetDestination(Player player, bool n, bool s, bool e, bool w)
+    public int[] GetDestination(int id, bool n, bool s, bool e, bool w)
     {
       // n = 0| s = 1| e = 2| w = 3
-      int destx = player.X;
-      int desty = player.Y;
-      if(n)
+      Player target = _db.Players.FirstOrDefault(p=>p.PlayerId==id);
+      int[] output = {target.X,target.Y,target.Z};
+       if(n)
       {
-        desty+=1;
+        output[1]-=1;
       }
       if(s)
       {
-        desty-=1;
+        output[1]+=1;
       }
       if(e)
       {
-        destx+=1;
+        output[0]+=1;
       }
       if(w)
       {
-        destx-=1;
+        output[0]-=1;
       }
       
-      if(desty<player.Y)
+      if(output[1]<target.Y)
       {
-        player.Bearing=1;
+        target.Bearing=1;
       }
-      if(desty>player.Y)
+      if(output[1]>target.Y)
       {
-        player.Bearing=0;
+        target.Bearing=0;
       }
-      if(destx<player.X)
+      if(output[0]<target.X)
       {
-        player.Bearing=3;
+        target.Bearing=3;
       }
-      if(destx>player.X)
+      if(output[0]>target.X)
       {
-        player.Bearing=2;
+        target.Bearing=2;
       }
-      _db.Entry(player).State = EntityState.Modified;
-    return(new int[] {destx, desty, player.Z});
+      _db.Entry(target).State = EntityState.Modified;
+    return output;
     }
   }
 }
--- EFMRN2/wwwroot/js/GameLoading.js
+++ EFMRN2/wwwroot/js/GameLoading.js
@@ -8,35 +8,57 @@ document.addEventListener('keyup',(e)=>{delete keysDown[e.keyCode];}, false);
 
 
 let dim = 7;
-
+canvas.parentNode.style.position = "fixed";
+canvas.parentNode.style.left = "25%";
 canvas.parentNode.style.height = window.innerHeight*0.8+"px";
 canvas.parentNode.style.width = canvas.parentNode.style.height;
-canvas.style.height = canvas.parentNode.style.height;
-canvas.style.width = canvas.style.height;
-TilesDavis.style.width = canvas.style.height;
+canvas.height = window.innerHeight*0.8;
+canvas.width = canvas.height;
+canvas.style.border = "solid black 1px";
+canvas.style.position = "fixed";
+canvas.style.left = "25%";
+TilesDavis.style.width = canvas.height+"px";
 TilesDavis.style.height = TilesDavis.style.width;
 TilesDavis.style.display = "grid";
 TilesDavis.style.gridTemplateColumns="repeat("+dim+",1fr)";
-canvas.style.position = "absolute";
+TilesDavis.style.position="absolute";
+
+console.log(canvas.height);
+console.log(canvas.width);
 
 let q = canvas.height/dim;
 
-setInterval(go, 1000);
-var PlayerId = 2;
+setInterval(go, 1000/6);
+var PlayerId = 3;
 
-function go()
+async function go()
 {
   let smellyPomegranite = takeInput();
   let apiString = 'http://localhost:5000/api/Game/move/?pid='+PlayerId+'&n='+smellyPomegranite[0]+'&s='+smellyPomegranite[1]+'&e='+smellyPomegranite[2]+'&w='+smellyPomegranite[3]+'';
   console.log(apiString);
-  fetch(apiString);
-  updateMap();
+  var back = await getMove(apiString);
+  console.log(back);
+  DrawSelf(back);
+  updateMap2();
 }
 
 function takeInput(){
   let keyStates=[(38 in keysDown ),(40 in keysDown),(39 in keysDown),(37 in keysDown)];
   console.log(keyStates)
   return keyStates;
+}
+
+async function updateMap2()
+{
+  let bucket = await getLocalTiles(PlayerId);
+  console.log(bucket);
+  let counter = 0;
+  let IncomingData = Array.from(document.getElementById("TileDivs").children);
+  bucket.forEach(e=>{
+    IncomingData[counter].classList = e.texture;
+    console.log(e.texture);
+    counter++;
+  });
 }
 
 async function updateMap()
@@ -52,10 +74,19 @@ async function updateMap()
     // square.style.width = q+"px";
     // square.style.height = q+"px";
     let coVariables = convertLinear(dim,parseInt(IncomingData[i].id));
-    let target = await getTile(coVariables[0],coVariables[1],z);
+    let target = await getTile(coVariables[0],y+3+coVariables[1],z);
     IncomingData[i].classList = target.texture;
   }
   
+}
+
+function DrawSelf(input)
+{
+  brush.clearRect(0,0,canvas.width, canvas.height);
+  brush.fillStyle = input.color;
+  brush.beginPath();
+  brush.arc(3.5*q,3.5*q,q/2,0,Math.PI*2,false);
+  brush.fill();
 }
 
 function convertLinear(x,l)
@@ -63,10 +94,34 @@ function convertLinear(x,l)
   let output = [l%x, ((l-(l%x))/x)+1]
   return output;
 }
+async function getMove(string)
+{
+  try{
+    const response = await fetch(string);
+    if(!response.ok) {
+        throw Error(response.statusText);        
+      }
+      return response.json();
+    } catch(error) {  
+      return error.message;
+    }
+}
 async function getPlayer(id)
 {
   try{
     const response = await fetch(`http://localhost:5000/api/Game/player/?id=${id}`);
+    if(!response.ok) {
+        throw Error(response.statusText);        
+      }
+      return response.json();
+    } catch(error) {  
+      return error.message;
+    }
+}
+async function getLocalTiles(id)
+{
+  try{
+    const response = await fetch(`http://localhost:5000/api/Game/fMap/?pid=${id}&range=3`);
     if(!response.ok) {
         throw Error(response.statusText);        
       }
