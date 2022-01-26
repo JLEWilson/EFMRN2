fc2e70285ef19622075ca9794dbc86b1e4c9e131
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-26 12:28:04 -0900

    WIP Audio

--- EFMRN2/wwwroot/js/AudioManager.js
+++ EFMRN2/wwwroot/js/AudioManager.js
@@ -1,34 +1,27 @@
-import $ from "jquery";
-
-export default class AudioManager{
-  constructor(){}
-
-  static setVolumeLevels(){
-    const audio1 = document.getElementById('over-world-audio');
-    audio1.volume = 0.1;
-  }
-  static startAudio(string, bool){
-    let audio = document.getElementById(string);
-    audio.loop = bool;
-    audio.currentTime = 0;
-    if(audio.paused){
-      audio.play();
-    }
-  }
-  static pauseAudio(string){
-    const audio = document.getElementById(string);
-    if(!audio.paused){
-      audio.pause();
-    }
-  }
-  static continueAudio(string){
-    const audio = document.getElementById(string);
-    if(audio.paused){
-      audio.play();
-    }
+function setVolumeLevels(){
+  const audio1 = document.getElementById('main-theme');
+  console.log(audio1);
+  audio1.volume = 0.1;
+}
+function startAudio(string, bool){
+  let audio = document.getElementById(string);
+  audio.loop = bool;
+  audio.currentTime = 0;
+  if(audio.paused){
+    audio.play();
   }
 }
-
-$(document).ready(() => {
-  setVolumeLevels();
-});
+function pauseAudio(string){
+  const audio = document.getElementById(string);
+  if(!audio.paused){
+    audio.pause();
+  }
+}
+function continueAudio(string){
+  const audio = document.getElementById(string);
+  if(audio.paused){
+    audio.play();
+  }
+}
+setVolumeLevels();
+startAudio("main-theme", true);
